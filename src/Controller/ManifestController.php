0114338fff8f193b0743b9a5c5d352056de20763
Debian <bitnami@ip-172-26-6-13.ec2.internal>
2023-07-27 13:53:59 +0000

    feat: add version

--- src/Controller/ManifestController.php
+++ src/Controller/ManifestController.php
@@ -6,11 +6,19 @@ use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class ManifestController extends ControllerBase {
-  public function generateManifest($node) {
-    $node = \Drupal::entityTypeManager()->getStorage('node')->load($node);
+  public function generateManifest($version, $node) {
+
     // TODO: Implement logic to generate manifest based on the node.
     // You may need to use other Drupal APIs or custom code to do so.
     $manifest = [];  // Replace with actual manifest.
+
+    if($version == "2") {
+      $manifest["@context"] = "http://iiif.io/api/presentation/2/context.json";
+    } elseif($version == "3") {
+      $manifest["@context"] = "http://iiif.io/api/presentation/3/context.json";
+    } else {
+      return new JsonResponse(['error' => 'Invalid version.'], 400);
+    }
 
     return new JsonResponse($manifest);
   }
